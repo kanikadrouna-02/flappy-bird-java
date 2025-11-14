9310426244ce2a9a7b08f084aad058c4c79baa47
kanikadrouna-02 <kanikad.ug24.cs@nitp.ac.in>
2025-11-14 20:38:18 +0530

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# flappy-bird-java
-a lightweight java swing recreation of the classic Flappy Bird - flap , dodge pipes, and chase your high score!!
---
+++ src/App.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 360;
+        int boardHeight =640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+
+        //so that the game dimension do not include the title bar 
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
+}
